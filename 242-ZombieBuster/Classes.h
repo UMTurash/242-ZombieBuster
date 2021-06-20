718d7a3186f3faee6fc3908563dbe5802a1cb9ff
MoodMojo <MoodMojo@users.noreply.github.com>
2021-06-20 17:57:20 +0300

    Added code for Entity and Warrior classes. Debugging pending.

--- 242-ZombieBuster/Classes.h
+++ 242-ZombieBuster/Classes.h
@@ -1,28 +1,51 @@
 #pragma once
 
 #include <iostream>
-
+using namespace std;
 class Entity
 {
+private:
+	int size;
+	char representation;
+public:
+	Entity(){ size = 0; representation = '0'; }
+	Entity(int s, int c) { size = s; representation = c; }
+	int getSize() { return size; }
+	char getSymbol() { return representation; }
+	void setSize(int s) { size = s; }
+	void setSymbol(char c) { representation = c; }
 };
 
-class Warrior
+class Warrior : Entity
 {
+private:
+	int life;
+	int ammunition;
+public:
+	Warrior() { life = 0; ammunition = 0; }
+	Warrior(int l, int a) { life = l; ammunition = a; }
+	int getLife() { return life; }
+	int getAmmo() { return ammunition; }
+	void setLife(int l) { life = l; }
+	void setAmmo(int a) { ammunition = a; }
+	void updateAmmo(int ammo) { ammunition -= ammo; }
 };
 
-class Derrik : Warrior
+class Derick : Warrior
 {
+public:
+	Derick() { this->setAmmo(30); this->setLife(100); this->setSymbol('D'); this->setSize(1); }
 };
 
 class Chichonne : Warrior
 {
 public:
-	void updateAmmo() { std::cout << "Yes"; }
+	Chichonne() { this->setAmmo(25); this->setLife(100); this->setSymbol('C'); this->setSize(1); }
 };
 
 
-//My Part
-class Zombie
+
+class Zombie : Entity
 {
 private:
 	int intLife;
@@ -30,9 +53,9 @@ private:
 	int intScore;
 };
 
-class Resources
+class Resources : Entity
 {
 public:
 	void Effect(Chichonne c, char effect) { c.updateAmmo(); }
-	void Effect(Derrik d, char effect) { std::cout << "No"; }
+	void Effect(Derick d, char effect) { cout << "No"; }
 };
