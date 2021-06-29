4c6a872a231f1110fdfafb93b8e02e301e5bba97
MoodMojo <MoodMojo@users.noreply.github.com>
2021-06-29 13:38:01 +0300

    Added a Grid class as well as Grid functions to work with the main game

--- 242-ZombieBuster/Classes.h
+++ 242-ZombieBuster/Classes.h
@@ -1,9 +1,212 @@
 #pragma once
 
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>   
 using namespace std;
+struct GridCell
+{
+	char symbol = '_';
+	int index = -1;
+	bool hidden = true;
+	bool occupied = false;
+};
+class Grid
+{
+private:
+	int size, zombieNum, medNum, resNum;
+	GridCell** cells;
+	int* indexList;
+public:
+	Grid(int n)
+	{
+		size = n;
+		zombieNum = 2 * (n * n / 25);
+		medNum = 3 * (n * n / 25);
+		resNum = 2 * (n * n / 25);
+		int Z = zombieNum;
+		int M = medNum;
+		int R = resNum;
+		cells = new GridCell* [size];
+		for (int i = 0; i < size; ++i)
+			cells[i] = new GridCell[size];
+		indexList = (int*)malloc(sizeof(int) * n * n);
+		int currentIndex = 0;
+		while (!(Z == 0 && M == 0 && R == 0))
+		{
+			int positionX = rand() % size;
+			int positionY = rand() % size;
 
+			if (cells[positionX][positionY].occupied == true)
+				continue;
+			if (Z != 0)
+			{
+				/*
+				0 = small zombie
+				1 = medium zombie
+				2 = large zombie
+				*/
+				int zType = rand() % 3;
+				if (zombieNum == 2)
+				{
+					if (Z == 2)
+						zType = 0;
+					else if(Z == 1)
+						zType = 1;
+				}
+				if (zombieNum == 3)
+				{
+					if (Z == 3)
+						zType = 0;
+					else if (Z == 2)
+						zType = 1;
+					else if (Z == 1)
+						zType = 2;
+				}
 
+				/*Make Zombies*/
+				/* Orientation of angle adjustments. Consider origin as (x,y)
+				-1,1	0,1		1,1
+				-1,0	0,0		1,0
+				-1,-1	0,-1	1,-1
+				*/
+				if (zType == 0)
+				{
+					cells[positionX][positionY].symbol = 'S';
+					cells[positionX][positionY].index = currentIndex;
+					cells[positionX][positionY].occupied = true;
+					currentIndex++;
+				}
+				else if (zType == 1)
+				{
+					int angleX = rand() % 3 -1;
+					int angleY = rand() % 3 -1;
+					if ((angleX == 0 && angleY == 0) || (cells[positionX+angleX][positionY+angleY].occupied == true))
+						continue;
+					cells[positionX][positionY].symbol = 'M';
+					cells[positionX][positionY].index = currentIndex;
+					cells[positionX][positionY].occupied = true;
+					cells[positionX+angleX][positionY+angleY].symbol = 'M';
+					cells[positionX+angleX][positionY+angleY].index = currentIndex;
+					cells[positionX+angleX][positionY+angleY].occupied = true;
+					currentIndex++;
+				}
+				else if (zType == 2)
+				{
+					int angleX = rand() % 3 - 1;
+					int angleY = rand() % 3 - 1;
+					int angleX1 = rand() % 3 - 1;
+					int angleY1 = rand() % 3 - 1;
+					if ((angleX == 0 && angleY == 0) || (cells[positionX + angleX][positionY + angleY].occupied == true))
+						continue;
+					if ((angleX1 == 0 && angleY1 == 0) || (cells[positionX + angleX1][positionY + angleY1].occupied == true))
+						continue;
+					cells[positionX][positionY].symbol = 'L';
+					cells[positionX][positionY].index = currentIndex;
+					cells[positionX][positionY].occupied = true;
+					cells[positionX + angleX][positionY + angleY].symbol = 'L';
+					cells[positionX + angleX][positionY + angleY].index = currentIndex;
+					cells[positionX + angleX][positionY + angleY].occupied = true;
+					cells[positionX + angleX1][positionY + angleY1].symbol = 'L';
+					cells[positionX + angleX1][positionY + angleY1].index = currentIndex;
+					cells[positionX + angleX1][positionY + angleY1].occupied = true;
+					currentIndex++;
+				}
+				Z--;
+			}
+			else if (M != 0)
+			{	
+				/*
+				0 = small MedKit
+				1 = medium MedKit
+				*/
+				int mType = rand() % 2;
+
+				/*Make Medkits*/
+				/* Orientation of angle adjustments. Consider origin as (x,y)
+				-1,1	0,1		1,1
+				-1,0	0,0		1,0
+				-1,-1	0,-1	1,-1
+				*/
+				if (mType == 0)
+				{
+					cells[positionX][positionY].symbol = '+';
+					cells[positionX][positionY].index = currentIndex;
+					cells[positionX][positionY].occupied = true;
+					currentIndex++;
+				}
+				else if (mType == 1)
+				{
+					int angleX = rand() % 3 - 1;
+					int angleY = rand() % 3 - 1;
+					if ((angleX == 0 && angleY == 0) || (cells[positionX + angleX][positionY + angleY].occupied == true))
+						continue;
+					cells[positionX][positionY].symbol = '*';
+					cells[positionX][positionY].index = currentIndex;
+					cells[positionX][positionY].occupied = true;
+					cells[positionX + angleX][positionY + angleY].symbol = '*';
+					cells[positionX + angleX][positionY + angleY].index = currentIndex;
+					cells[positionX + angleX][positionY + angleY].occupied = true;
+					currentIndex++;
+				}
+				M--;
+			}
+			else if (R != 0)
+			{
+				cells[positionX][positionY].symbol = '+';
+				cells[positionX][positionY].index = currentIndex;
+				cells[positionX][positionY].occupied = true;
+				currentIndex++;
+				R--;
+			}
+		}
+	}
+
+	int moveGrid(int direction)
+	{
+
+	}
+	char searchIndex(int index)
+	{
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				if (cells[i][j].index == index)
+				{
+					return cells[i][j].symbol;
+				}
+			}
+		}
+	}
+	void killIndex(int index)
+	{
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				if (cells[i][j].index == index)
+				{
+					cells[i][j].symbol = 'X';
+				}
+			}
+		}
+	}
+	void printGrid() 
+	{
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				if(cells[i][j].hidden == true)
+					printf("\t_\t");
+				else
+					printf("\t%c\t", cells[i][j].symbol);
+			}
+			printf("\n");
+		}
+	}
+};
 class Entity
 {
 private:
